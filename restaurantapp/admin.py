431413b6723b831b78e4bee25fdbf8388e65eb31
Mohd Razzeq <rzq272@gmail.com>
2023-07-14 15:17:13 +0800

    Add : Reservation model

--- restaurantapp/admin.py
+++ restaurantapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from restaurantapp.models import Drinks
+from restaurantapp.models import Drinks, DrinksCategory, Reservation
 
 # Register your models here.
 admin.site.register(Drinks)
+admin.site.register(DrinksCategory)
+admin.site.register(Reservation)
--- restaurantapp/models.py
+++ restaurantapp/models.py
@@ -4,4 +4,18 @@ from django.db import models
 class Drinks(models.Model):
     drink_name = models.CharField(max_length=200)
     price = models.IntegerField()
+    category_id = models.ForeignKey("DrinksCategory", default=None, on_delete=models.PROTECT)
+    
+class DrinksCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+    
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    guest_count = models.IntegerField()
+    reservation_time = models.DateField(auto_now=True)
+    comments = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return f"{self.first_name}, {self.last_name}"
     
